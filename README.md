31507439207fd758f957db8205c8909b06f53ae9
imnarin <imnarin0316@gmail.com>
2024-03-03 13:39:08 +0900

    feat: 폴더변경

---
+++ README.md
@@ -0,0 +1,24 @@
+# 2023년 포트폴리오(포폴용)
+2023년용으로 제작된 포트폴리오 입니다. 
+
+## 개발환경
+
+ReactJs\
+Javascript\
+Typescript\
+SCSS\
+styled-components\
+style module\
+styled-reset\
+IntersectionObserver API\
+swiper slider
+
+
+## 반응형 
+
+@media, @get-vw 활용됨.
+
+## Figma
+https://www.figma.com/file/rZJIMsUqztYHT5Uu0L0362/2023-%ED%8F%AC%ED%8A%B8%ED%8F%B4%EB%A6%AC%EC%98%A4?node-id=0%3A1&t=ROukiRPnzGEcp9Iu-1
+
+
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./common/GlobalStyle";
+import Main from "./pages/Main/Main";
+import Layout from "./components/Layout/Layout";
+import About from "./components/About/About";
+import NotFound from "./components/NotFound/NotFound";
+import Career from './components/Career/Career';
+import Portfolio from './components/Portfolio/Portfolio';
+
+const App = () => {
+  document.cookie = "safeCookie1=foo; SameSite=Lax"; 
+document.cookie = "safeCookie2=foo";  
+document.cookie = "crossCookie=bar; SameSite=None; Secure";
+
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Main />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/career" element={<Career />}></Route>
+            <Route path="/portfolio" element={<Portfolio />}></Route>
+            {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+
+          {/* <Main /> */}
+          {/* <About /> */}
+        </Layout>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
